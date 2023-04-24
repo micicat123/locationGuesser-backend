0fdf5e73b6752cd69aca831d9f5a52dd60e0c0f9
Micicat123 <mici.zep@gmail.com>
2023-04-24 10:13:32 +0200

    trying to connect to s3

--- src/modules/upload/S3.service.ts
+++ src/modules/upload/S3.service.ts
@@ -1,11 +1,12 @@
 import { Req, Res, Injectable } from '@nestjs/common';
 import * as multer from 'multer';
+import { S3 } from 'aws-sdk/clients/all';
 import * as AWS from 'aws-sdk';
 import * as multerS3 from 'multer-s3';
 
 const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
-const s3 = new AWS.S3();
-AWS.config.update({
+const s3 = new S3({
+  region: 'eu-north-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
+import { ImageUploadService } from './S3.service';
 
 @Module({
-  providers: [],
+  providers: [ImageUploadService],
   controllers: [UploadController]
 })
 export class UploadModule {}
